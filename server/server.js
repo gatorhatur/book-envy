ebe40921e2193eaf75bd46b5cb1957e659aa9e7d
Brandon Davis <brancdavis@gmail.com>
2022-10-25 15:18:49 -0400

    initial setup and integration of auth into apollo server

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
+const { typeDefs, resolvers } = require('./schemas');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+})
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+const startApolloServer = async (typeDefs, resolers) => {
+
+  server.applyMiddleware({ app });
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  //app.use(routes);
+
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  });
+  
+}
+
+startApolloServer(typeDefs, resolveers);
